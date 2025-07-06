c4e56c76c742058f591731d4767dffd7a19c6035
Dimitri Lavrenük <20122620+dlavrenuek@users.noreply.github.com>
2025-07-06 14:12:01 +0200

    use zod for validation and type safety

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # proper-conf
+
+Work in progress. Type-safe and validated configuration for your application. Inspired by Spring Boot's externalized
+configuration handling (especially [ENV variable handling](https://docs.spring.io/spring-boot/reference/features/external-config.html#features.external-config.files.env-variables))
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,25 @@
-type ParserProps = {};
+import { z } from "zod";
 
-type ValueParser = <Type>(props: ParserProps) => Type;
+const ConfigSchema = z.object({
+  database: z.object({
+    url: z.string(),
+    name: z.string(),
+    password: z.string(),
+  }),
+  users: z.array(
+    z.object({
+      email: z.optional(z.string()),
+      name: z.string(),
+    }),
+  ),
+  groups: z.array(z.string()),
+});
 
-type ValueArrayType = <Type>(
-  valueParser: (props: ParserProps) => Type,
-) => (props: ParserProps) => Type[];
+console.log(ConfigSchema.shape);
 
-export const string = () => () => "";
-
-export const number = () => () => 123;
-
-export const array =
-  <Type>(valueParser: (props: ParserProps) => Type) =>
-  (): Type[] => [];
-
-type Conf = {
-  [key: string]: ((props: ParserProps) => unknown) | Conf;
-};
-
-type Result<C extends Conf> = {
-  [key in keyof C]: C[key] extends Conf
-    ? Result<C[key]>
-    : C[key] extends () => infer R
-      ? R
-      : never;
-};
-
-// @ts-ignore
-const properConf = <C extends Conf>(conf: C): Result<C> => null;
-
-const test = properConf({
-  port: number(),
-  addr: string(),
-  whitelist: array(string()),
-  someObj: {
-    asd: () => new Date(),
+ConfigSchema.parse({
+  database: {
+    url: "123",
+    name: "",
   },
 });
